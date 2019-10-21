56d7830c1b09acf2d127a28aed4e6935979a0acf
rheajoywalters <54914771+rheajoywalters@users.noreply.github.com>
2019-10-21 12:43:06 -0600

    initial commit for album app

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyDnT749pto7K9C5N6VvS0aTVvFMJq6QSzI",
-  authDomain: "authentication-d0e33.firebaseapp.com",
-  databaseURL: "https://authentication-d0e33.firebaseio.com",
-  projectId: "authentication-d0e33",
-  storageBucket: "",
-  messagingSenderId: "310675757578",
-  appId: "1:310675757578:web:9e17f766afbce2075bddf3"
+  apiKey: "AIzaSyAu4SMLsofpnCLutL8NVW_VXYv58CaVfd4",
+  authDomain: "album-baeb8.firebaseapp.com",
+  databaseURL: "https://album-baeb8.firebaseio.com",
+  projectId: "album-baeb8",
+  storageBucket: "album-baeb8.appspot.com",
+  messagingSenderId: "707004152258",
+  appId: "1:707004152258:web:9c346f77cb6aea3f47fe5b",
+  measurementId: "G-2Y27DNE7GJ"
 };
 
 firebase.initializeApp(config);
